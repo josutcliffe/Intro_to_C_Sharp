f2ee2d59a82f3665370f4ee2218a1ecaa51e7851
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-08-16 07:15:08 +0800

    Exercise 7 - simple addition/multiplication script.

---
+++ Session_04_Ex07/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Create a mathematical function that takes two parameters that are numeric types
+//and performs a basic arithmetic function on them and prints the result to the console.
+//Try addition and multiplication.
+
+int mathAddFunction(int a, int b)
+{ return a + b;
+}
+
+int resultAdd = mathAddFunction(5, 5);
+
+Console.WriteLine("The result of adding " + 5 + " and " + 5 + " is: " + resultAdd);
+
+int mathMultiplyFunction(int a, int b)
+{
+    return a * b;
+}
+
+int resultMultiply = mathMultiplyFunction(5, 5);
+
+Console.WriteLine("The result of multiplying " + 5 + " and " + 5 + " is: " + resultMultiply);
