889251dbb003a41e79e4843e24078deee59394fd
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-08-16 06:53:46 +0800

    Functions - Exercise 06 setup

---
+++ Session_04_Ex06/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Create a welcome message function called WelcomeMessage().
+// When called it will display the program’s welcome message.
+// This will call several Console.WriteLine() functions to print an opening message. E.g.:
+//********************************
+//Welcome to My Application!
+//********************************
+
