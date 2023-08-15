18c0c646e322f9d34642b601baaa7b8e6214505e
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-08-16 06:42:06 +0800

    Simple times table script.

--- Session_03_Ex04/Program.cs
+++ Session_03_Ex04/Program.cs
@@ -1,5 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Please enter a number from 1-12:");
+Console.WriteLine("Please enter a number from 0-12:");
 
-int userMonth = Convert.ToInt32(Console.ReadLine());
+int number = Convert.ToInt32(Console.ReadLine());
 
+if (number >= 0 && number <= 12)
+    {
+        Console.WriteLine("You selected the number " + number + ".");
+        for (int i = 0; i <= 12; i++)
+    {
+        Console.WriteLine(number + " x " + i + " = " + (number * i));
+    }
+}
+else
+{
+    Console.WriteLine("Please select a number from 0-12.");
+    }
