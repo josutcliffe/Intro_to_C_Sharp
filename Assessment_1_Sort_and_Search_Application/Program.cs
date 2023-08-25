1931e957d3d534ec3a509324f5ae98a8167918a9
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-08-25 18:55:50 +0800

    Updating user inputs.

--- Assessment_1_Sort_and_Search_Application/Program.cs
+++ Assessment_1_Sort_and_Search_Application/Program.cs
@@ -1,5 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-int rangeLottery = 5;
+int totalNumbers = 5;
+
+int rangeFirstNumber = 0;
+
+int rangeLastNumber = 10;
 
 Console.WriteLine("Welcome to The Lottery Game");
-Console.WriteLine("Please enter " + rangeLottery + " numbers to play The Lottery Game");
+Console.WriteLine("Please enter " + totalNumbers + " numbers between " + rangeFirstNumber + " and " + rangeLastNumber + ".");
+
+//int userNumbers = Convert.ToInt32(Console.ReadLine());
+
+//Console.WriteLine("Please enter a number from 0-12:");
+
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//if (number >= 0 && number <= 12)
+//{
+//    Console.WriteLine("You selected the number " + number + ".");
+//    for (int i = 0; i <= 12; i++)
+//    {
+//        Console.WriteLine(number + " x " + i + " = " + (number * i));
+//    }
+//}
+//else
+//{
+//    Console.WriteLine("Please select a number from 0-12.");
+//}
