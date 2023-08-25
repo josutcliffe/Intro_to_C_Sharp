835ae130270ab07a6eba82b8dfb1cb1882b33f98
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-08-25 18:47:33 +0800

    Building user input for The Lottery Game

---
+++ Assessment_1_Sort_and_Search_Application/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int rangeLottery = 5;
+
+Console.WriteLine("Welcome to The Lottery Game");
+Console.WriteLine("Please enter " + rangeLottery + " numbers to play The Lottery Game");
