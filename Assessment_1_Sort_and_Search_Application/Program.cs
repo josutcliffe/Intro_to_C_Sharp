1b3bb42c23163faa8c7d23f214ca8a3ae1f0226b
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-08-26 16:09:45 +0800

    Updating user input: user can input now choose their numbers.

--- Assessment_1_Sort_and_Search_Application/Program.cs
+++ Assessment_1_Sort_and_Search_Application/Program.cs
@@ -1,14 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//====================================
+//Student: Joshua Sutcliffe
+//Student ID: 20107131
+//Student email: 20107131@tafe.wa.edu.au
+//====================================
+
 int totalNumbers = 5;
+int rangeMin = 0;
+int rangeMax = 20;
 
-int rangeFirstNumber = 0;
+int[] userNumbers = new int[totalNumbers];
+int[] randomNumbers = new int[totalNumbers];
 
-int rangeLastNumber = 10;
+// int [] defaultNumberArray = net int[3] {3, 6, 9}; //for use when testing application
 
-Console.WriteLine("Welcome to The Lottery Game");
-Console.WriteLine("Please enter " + totalNumbers + " numbers between " + rangeFirstNumber + " and " + rangeLastNumber + ".");
+void GetUserNumbers()
+{
+    Console.WriteLine("Welcome to The Lottery Game");
+    Console.WriteLine("Please enter " + totalNumbers + " numbers between " + rangeMin + " and " + rangeMax + ".");
+
+    for (int i = 0; i < userNumbers.Length; i++)
+    {
+        Console.WriteLine($"Enter Value " + i + ":");
+        string userInput = Console.ReadLine();
+        if (!int.TryParse(userInput, out userNumbers[i]) || userNumbers[i] < rangeMin || userNumbers[i] > rangeMax)
+        {
+            //user input error checking
+            Console.WriteLine("You did not select a valid number. Please try again.");
+            i--;
+        }
+    }
+}
+
+GetUserNumbers();
 
 //int userNumbers = Convert.ToInt32(Console.ReadLine());
+
 
 //Console.WriteLine("Please enter a number from 0-12:");
 
@@ -25,4 +52,65 @@ Console.WriteLine("Please enter " + totalNumbers + " numbers between " + rangeFi
 //else
 //{
 //    Console.WriteLine("Please select a number from 0-12.");
+//}
+
+
+//using System;
+
+//class Program
+//{
+//    static void Main()
+//    {
+//        // Set the number of elements in the arrays
+//        int arraySize = 10;
+
+//        // Set the range of random numbers
+//        int minValue = 1;
+//        int maxValue = 101;
+
+//        // Prompt user to enter a list of numbers
+//        int[] userNumbers;
+//        while (true)
+//        {
+//            Console.WriteLine("Enter a list of " + arraySize + " numbers separated by spaces: ");
+//            string input = Console.ReadLine();
+//            userNumbers = Array.ConvertAll(input.Split(' '), int.Parse);
+
+//            if (userNumbers.Length == arraySize)
+//                break;
+
+//            Console.WriteLine("Warning: You entered fewer than " + arraySize + " numbers. Please try again.");
+//        }
+
+//        // Sort the userNumbers array
+//        Array.Sort(userNumbers);
+
+//        // Generate a random list of numbers
+//        Random rand = new Random();
+//        int[] randomNumbers = new int[arraySize];
+//        for (int i = 0; i < randomNumbers.Length; i++)
+//            randomNumbers[i] = rand.Next(minValue, maxValue);
+
+//        // Sort the randomNumbers array
+//        Array.Sort(randomNumbers);
+
+//        // Print the randomNumbers array
+//        Console.WriteLine("Random numbers: " + string.Join(" ", randomNumbers));
+
+//        // Compare the two arrays using binary search
+//        bool areEqual = CompareArrays(userNumbers, randomNumbers);
+//        Console.WriteLine("The two arrays are " + (areEqual ? "equal" : "not equal"));
+//    }
+
+//    static bool CompareArrays(int[] array1, int[] array2)
+//    {
+//        if (array1.Length != array2.Length)
+//            return false;
+
+//        for (int i = 0; i < array1.Length; i++)
+//            if (Array.BinarySearch(array2, array1[i]) < 0)
+//                return false;
+
+//        return true;
+//    }
 //}
