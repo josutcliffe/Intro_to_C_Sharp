0a33324fdfeb773fdc055a2ad5751dcbfb22fd48
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-09-03 12:14:04 +0800

    Updated GetUserNumbers to not allow repeated numbers. Updated LinearSearch to give user feedback on if their number is in the randomNumbers array.

--- Assessment_1_Sort_and_Search_Application/Program.cs
+++ Assessment_1_Sort_and_Search_Application/Program.cs
@@ -6,13 +6,13 @@
 //====================================
 
 int totalNumbers = 5;
-int rangeMin = 0;
+int rangeMin = 1;
 int rangeMax = 20;
 
 int[] userNumbers = new int[totalNumbers];
 int[] randomNumbers = new int[totalNumbers];
 
-// int [] defaultNumberArray = net int[3] {3, 6, 9}; //for use when testing application
+//int [] defaultNumberArray = new int[5] {1, 3, 6, 7, 9}; //for use when testing application
 
 void GetUserNumbers()
 {
@@ -21,104 +21,124 @@ void GetUserNumbers()
 
     for (int i = 0; i < userNumbers.Length; i++)
     {
-        Console.WriteLine($"Enter Value " + i + ":");
+        Console.WriteLine($"Enter value " + i + ":");
         string userInput = Console.ReadLine();
         if (!int.TryParse(userInput, out userNumbers[i]) || userNumbers[i] < rangeMin || userNumbers[i] > rangeMax)
         {
-            //user input error checking
-            Console.WriteLine("You did not select a valid number. Please try again.");
+            Console.WriteLine("You did not select a valid number. Please try again.");  //user input error checking
             i--;
         }
+        else
+        {
+            bool repeatedNumber = false;  //checking for repeated numbers
+            for (int j = 0; j < i; j++)
+            {
+                if (userNumbers[j] == userNumbers[i])
+                {
+                    repeatedNumber = true;
+                    break;
+                }
+            }
+            if (repeatedNumber)
+            {
+                Console.WriteLine("You have already selected this number. Please choose a different number.");
+                i--;
+            }
+        }
     }
-    Console.WriteLine(userNumbers);
 }
 
 GetUserNumbers();
 
-void PrintArray(int[] array)
+
+void PrintArray(int[] arrayToPrint) //Function to print array values
 {
-    for (int i = 0; i < array.Length; i++)
+    for (int i = 0; i < arrayToPrint.Length; i++)
     {
-        if (i == array.Length - 1)
+        if (i == arrayToPrint.Length - 1)
         {
-            Console.WriteLine(array[i] + ".");
+            Console.WriteLine(arrayToPrint[i] + ".");
             continue;
         }
-        Console.Write(array[i] + ", ");
+        Console.Write(arrayToPrint[i] + ", ");
     }
 }
 
+//PrintArray(userNumbers);
+Array.Sort(userNumbers);
+Console.WriteLine("You chose the following numbers:");
 PrintArray(userNumbers);
 
 
-//random array creation
-for (int i = 0; i < randomNumbers.Length; i++)
+
+void RandomArrayCreate() //Random number array creation
 {
-    Random random = new Random();
-    randomNumbers[i] = random.Next(rangeMin, rangeMax + 1);
+    for (int i = 0; i < randomNumbers.Length; i++)
+    {
+        // TODO: edit/update to make sure no numbers are repeated in the randomNumbers array
+        Random random = new Random();
+        randomNumbers[i] = random.Next(rangeMin, rangeMax + 1);
+    }
 }
 
+RandomArrayCreate();
+
+
+Array.Sort(randomNumbers);
+Console.WriteLine("The random lottery numbers were:");
 PrintArray(randomNumbers);
+
+
+
+
+void LinearSearch(int[] firstArray, int[] secondArray)  //Use a linear search to compare the values between two arrays.
+{
+    if (firstArray.Length == secondArray.Length)
+    {
+        for (int i = 0; i < firstArray.Length; i++)
+        {
+            bool correctNumber = false;
+            for (int j = 0; j < secondArray.Length; j++)
+            {
+                if (firstArray[i] == secondArray[j])
+                {
+                    Console.WriteLine("Your guessed number " + firstArray[i] + " is a winning lottery number.");
+                    correctNumber = true;
+                    break;
+                }
+                else
+                {
+                    if (firstArray[i] != secondArray[j])
+                    {
+                        Console.WriteLine("Your guessed number " + firstArray[i] + " is not a winning lottery number.");
+                    }
+                }
+            }
+        }
+    }
+}
+
+LinearSearch(userNumbers, randomNumbers);
 
 int.Parse(Console.ReadLine());  //force the terminal to stay open
 
 
-//using System;
-
-//class Program
+//int BinarySearch(int[] arrayToSearch, int value, int low, int high)
 //{
-//    static void Main()
+//    if (high >= low)
 //    {
-//        // Set the number of elements in the arrays
-//        int arraySize = 10;
+//        int mid = (high - low) / 2;  //Get mid value
 
-//        // Set the range of random numbers
-//        int minValue = 1;
-//        int maxValue = 101;
+//        if (arrayToSearch[mid] == value) return mid;  //Checks if value equals mid
 
-//        // Prompt user to enter a list of numbers
-//        int[] userNumbers;
-//        while (true)
-//        {
-//            Console.WriteLine("Enter a list of " + arraySize + " numbers separated by spaces: ");
-//            string input = Console.ReadLine();
-//            userNumbers = Array.ConvertAll(input.Split(' '), int.Parse);
+//        if (arrayToSearch[mid] > value) return BinarySearch(arrayToSearch, value, low, mid - 1);  //Determine is value is lower or higher than mid in the sorted array
 
-//            if (userNumbers.Length == arraySize)
-//                break;
-
-//            Console.WriteLine("Warning: You entered fewer than " + arraySize + " numbers. Please try again.");
-//        }
-
-//        // Sort the userNumbers array
-//        Array.Sort(userNumbers);
-
-//        // Generate a random list of numbers
-//        Random rand = new Random();
-//        int[] randomNumbers = new int[arraySize];
-//        for (int i = 0; i < randomNumbers.Length; i++)
-//            randomNumbers[i] = rand.Next(minValue, maxValue);
-
-//        // Sort the randomNumbers array
-//        Array.Sort(randomNumbers);
-
-//        // Print the randomNumbers array
-//        Console.WriteLine("Random numbers: " + string.Join(" ", randomNumbers));
-
-//        // Compare the two arrays using binary search
-//        bool areEqual = CompareArrays(userNumbers, randomNumbers);
-//        Console.WriteLine("The two arrays are " + (areEqual ? "equal" : "not equal"));
+//        Console.WriteLine("test");
+//        return BinarySearch(arrayToSearch, value, mid + 1, high);
 //    }
-
-//    static bool CompareArrays(int[] array1, int[] array2)
-//    {
-//        if (array1.Length != array2.Length)
-//            return false;
-
-//        for (int i = 0; i < array1.Length; i++)
-//            if (Array.BinarySearch(array2, array1[i]) < 0)
-//                return false;
-
-//        return true;
-//    }
+//    return -1;  // -1 in C# means we have not found the values it a non index number.
 //}
+
+//BinarySearch(GetUserNumbers, 3, 0, defaultNumberArray.Length - 1);
+
+//int.Parse(Console.ReadLine());  //force the terminal to stay open
