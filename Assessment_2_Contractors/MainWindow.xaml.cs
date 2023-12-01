64765bf4b27eca1d7db3c4c5efd7cb6aa8e6e620
Joshua Sutcliffe <joshua_sutcliffe@outlook.com>
2023-12-01 08:03:37 +0800

    Updated code based on UI test plan results.

--- Assessment_2_Contractors/MainWindow.xaml.cs
+++ Assessment_2_Contractors/MainWindow.xaml.cs
@@ -58,6 +58,16 @@ namespace Assessment_2_Contractors
 
         private void Button_AddContractor_Click(object sender, RoutedEventArgs e)
         {
+            //added validation of firstName to ensure only characters can be input. loops through each individual character and checks if character is a letter.
+            string firstName = TextBox_FirstName.Text;
+            foreach (char c in firstName)
+            {
+                if (!char.IsLetter(c))
+                {
+                    MessageBox.Show("First name should not contain any symbols. Please re-enter first name.");
+                    return;
+                }
+            }
             int id = recruitment.GetContractorCount();  //use the count to make the contractor ID the next number in the count
             Contractor newContractor = new Contractor(id, TextBox_FirstName.Text, TextBox_LastName.Text, (DateTime)DatePicker_Contractor.SelectedDate, (int)Slider_HourlyWage.Value, (ContractorAssignmentTypes)ContractorAssignmentTypes.Available, null, null);
             recruitment.AddContractor(newContractor);
@@ -78,15 +88,37 @@ namespace Assessment_2_Contractors
         ///TODO: add handling for when a contractor assigned to a job is removed, the job should then become unassigned.
         private void Button_RemoveContractor_Click(object sender, RoutedEventArgs e)
         {
+            
             if (ListBox_Contractors.SelectedItem != null)
             {
                 Contractor selectedContractor = (Contractor)ListBox_Contractors.SelectedItem;
+
+                //check through all jobs if the selected contractor is assigned. if assigned, reset the job to unassigned before
+                //removing contractor from contractor list
+                foreach (Job job in recruitment.GetJobs())
+                {
+                    if (job.AssignedContractor == selectedContractor)
+                    {
+                        job.JobAssignmentType = JobAssignmentTypes.Unassigned;
+                        job.AssignedContractor = null;
+                        job.AssignedContractorName = null;
+                    }
+                }
+
                 recruitment.RemoveContractor(selectedContractor);
+                
+            }
+            else
+            {
+                //User warning that no contractor is selected
+                MessageBox.Show("No contractor is selected. Please select a contractor then press Remove Contractor.");
             }
 
             //Refresh ListBoxes
             RefreshUnassignedContractorList();
             RefreshContractorList();
+            RefreshUnassignedJobList();
+            RefreshJobList();
         }
 
         private void RefreshJobList()
@@ -101,9 +133,17 @@ namespace Assessment_2_Contractors
 
         private void Button_AddJob_Click(object sender, RoutedEventArgs e)
         {
+
             double jobCost;
             if (double.TryParse(TextBox_JobCost.Text, out jobCost))  //ensure jobCost is entered correctly or it cannot be passed into the newJob
             {
+                //validate user input to make sure jobCost is 0 or greater
+                if (jobCost < 0)
+                {
+                    MessageBox.Show("Job cost must be 0 or greater.");
+                    return;
+                }
+
                 int jobID = recruitment.GetJobCount();  //use the count to make the job ID the next number in the count
                 Job newJob = new Job(jobID, TextBox_JobTitle.Text, (DateTime)DatePicker_Job.SelectedDate, jobCost, (JobStatusTypes)JobStatusTypes.Incomplete, (JobAssignmentTypes)JobAssignmentTypes.Unassigned, null, null);
                 recruitment.AddJob(newJob);
@@ -121,11 +161,27 @@ namespace Assessment_2_Contractors
 
         private void Button_GetUnassignedContractors_Click(object sender, RoutedEventArgs e)
         {
+            //if there are no available contractors in list when user clicks button, pop-up message will warn them
+            var availableContractors = recruitment.GetAvailableContractors();
+            if (availableContractors.Count == 0)
+            {
+                MessageBox.Show("There are no available contractors.");
+                return;
+            }
+
             ListBox_ContractorsUnassigned.ItemsSource = recruitment.GetAvailableContractors();
         }
 
         private void Button_GetUnassignedJobs_Click(object sender, RoutedEventArgs e)
         {
+            //if there are no available contractors in list when user clicks button, pop-up message will warn them
+            var unassignedJobs = recruitment.GetUnassignedJobs();
+            if (unassignedJobs.Count == 0)
+            {
+                MessageBox.Show("There are no unassigned jobs.");
+                return;
+            }
+
             ListBox_JobsUnassigned.ItemsSource = recruitment.GetUnassignedJobs();
         }
 
@@ -133,6 +189,21 @@ namespace Assessment_2_Contractors
         {
             Contractor availableContractor = (Contractor)ListBox_ContractorsUnassigned.SelectedItem;
             Job unassignedJob = (Job)ListBox_JobsUnassigned.SelectedItem;
+
+
+            //User warning that no contractor is selected
+            if (availableContractor == null)
+            {
+                MessageBox.Show("Please select a contractor.");
+                return;
+            }
+
+            //User warning that no job is selected
+            if (unassignedJob == null)
+            {
+                MessageBox.Show("Please select a job.");
+                return;
+            }
 
             recruitment.AssignJob(availableContractor, unassignedJob);
             
@@ -152,6 +223,13 @@ namespace Assessment_2_Contractors
                 recruitment.CompleteJob(completedJob);
             }
 
+            //User warning that no job is selected to complete
+            if (completedJob == null)
+            {
+                MessageBox.Show("Please select a job to complete.");
+                return;
+            }
+
             //Refresh ListBoxes
             RefreshContractorList();
             RefreshUnassignedContractorList();
@@ -161,9 +239,35 @@ namespace Assessment_2_Contractors
 
         private void Button_JobByCost_Click(object sender, RoutedEventArgs e)
         {
-            int minCost = int.Parse(TextBox_JobMinCost.Text);
-            int maxCost = int.Parse(TextBox_JobMaxCost.Text);
+            //added parsing and warning to check that input for minCost and maxCost is an integer
+            int minCost;
+            if (!int.TryParse(TextBox_JobMinCost.Text, out minCost))
+            {
+                MessageBox.Show("Please enter a valid cost for minimum cost.");
+                return;
+            }
+            int maxCost;
+            if (!int.TryParse(TextBox_JobMaxCost.Text, out maxCost))
+            {
+                MessageBox.Show("Please enter a valid cost for maximum cost.");
+                return;
+            }
+
+            //warn user is minCost is equal to or greater than maxCost
+            if (minCost >= maxCost)
+            {
+                MessageBox.Show("Minimum cost must be less than maximum cost.");
+                return;
+            }
+
             List<Job> jobs = recruitment.GetJobByCost(minCost, maxCost);
+
+            //warn user if there are no jobs with cost in the user cost range
+            if (jobs.Count == 0)
+            {
+                MessageBox.Show("There are no jobs in the specified cost range.");
+                return;
+            }
 
             ListBox_Jobs.ItemsSource = jobs;
         }
